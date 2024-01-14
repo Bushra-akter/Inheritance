56a9725774382d12ca551581f05944c58fcb4227
bushra <bushra@DESKTOP-4FR5LE5>
2024-01-14 13:03:10 +0600

    accessmodifier

---
+++ Access_modifier/src/defaultpractise/p2/P2.java
@@ -0,0 +1,13 @@
+package defaultpractise.p2;
+import defaultpractise.p1.*;
+
+class P2 {
+
+	public static void main(String[] args) {
+		P1 obj= new P1();   // here wrong(compile time error) because P1 is default. if P1 is public then it is working.
+		
+		
+
+	}
+
+}
